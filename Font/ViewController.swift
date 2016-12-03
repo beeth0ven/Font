4b4ace8c2f5b5da8ed9c060dfcb3ca9b0d201974
beeth0ven <beeth0vendev@163.com>
2016-12-03 17:36:59 +0800

    1

--- Font/ViewController.swift
+++ Font/ViewController.swift
@@ -21,7 +21,7 @@ class MenuTableViewController: UITableViewController {
     
 }
 
-class FTSplitViewControllerT: UISplitViewController, UISplitViewControllerDelegate {
+class FTSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
